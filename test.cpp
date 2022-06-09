cb2d5fe5cb856dd205586719cbd1c0322686282b
toraltai <flangkg@gmail.com>
2022-06-09 13:03:21 +0600

    git

---
+++ test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x, y;
+  int sum;
+  cout << "Type a : ";
+  cin >> x;
+  cout << "Type b : ";
+  cin >> y;
+  sum = x + y;
+  cout << "Sum is: " << sum << endl;
+  return 0;
+}
+
